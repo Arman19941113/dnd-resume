ae605b8fda8ca51b43c91055514b2b4700f75fd4
Arman Tang <td19941113@gmail.com>
2025-09-08 21:45:23 +0800

    chore: vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import * as Sentry from '@sentry/react'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
@@ -26,6 +28,8 @@ root.render(
   <StrictMode>
     <ErrorBoundary>
       <RouterProvider router={router} />
+      <Analytics />
+      <SpeedInsights sampleRate={0.2} />
     </ErrorBoundary>
   </StrictMode>,
 )
